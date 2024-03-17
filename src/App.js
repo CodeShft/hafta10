022fec88a2e88b51edf4fb07eb5a44e9a6679c6c
CodeShft <nileceyazilim@gmail.com>
2024-03-17 17:31:25 +0300

    cleanup function örnegi yapıldı

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import C1 from "./C1";
 
 function App() {
   const [c1Kapali, c1AcKapa] = useState(false)
-  const [sayac, setSayac] = useState(0)
-
-  function c1Kapat() {
-    c1AcKapa( eskiDeger => !eskiDeger )
-  }
 
   return (
     <>
-      <div className="container my-5 bg-info-subtle p-5">
-          <button onClick={c1Kapat} className="btn btn-warning btn-sm my-3">C1 KAPAT</button>
-
-          {!c1Kapali && <C1 sayac={sayac} setSayac={setSayac} />}
-          
+      <div className="container my-5">
+        <button className="btn btn-success" onClick={ ()=>{ c1AcKapa(eskiDeger=>!eskiDeger) } }>C1 Aç/Kapa</button>
+        {!c1Kapali && <C1 />}
       </div>
     </>
-
   );
 }
 
